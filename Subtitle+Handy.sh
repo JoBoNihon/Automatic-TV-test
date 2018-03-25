01bcad75b4c89b6289cd6a2d5123e6d3acae8cd4
Josep <josep@tranzas.co.jp>
2018-03-26 08:59:29 +0900

    Implementation Subtitle+Handy test

--- Subtitle+Handy.sh
+++ Subtitle+Handy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-adb shell input keyevent 141;
+#Import remote controller keys
+. ./RemoteController.sh
+
+$rDigital;
 sleep 3;
-adb shell input keyevent 8;
+$rNum1;
 sleep 3;
-adb shell input keyevent 140;
+$rSubtitle;
 sleep 3;
-adb shell input keyevent 134;
+$rHandy;
 sleep 3;
 #Test all subtitles the sizes
 for i in 0 1
 do
-    adb shell input keyevent 131;
+    $rSubtitleSize;
     sleep 2;
     #Test all subtitles positions with both sizes
     for i in 0 1
@@ -18,18 +21,18 @@ do
         #Test all the handy sizes
         for i in 0 1
         do
-            adb shell input keyevent 135;
+            $rHandySize;
             sleep 2;
             #Test all handy positions with both sizes
             for i in $(seq 0 3)
             do 
-                adb shell input keyevent 136;    
+                $rHandyPosition;    
                 sleep 2;
             done
         done
-        adb shell input keyevent 125;    
+        $rSubtitlePosition;    
         sleep 2;
     done
 done
 #Test finish
-adb shell input keyevent 134;
+$rHandy;
